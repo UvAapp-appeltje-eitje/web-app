9b591d94934d3a9e7190416e5ea6c9fc91bc2aa3
Noah Loomans <noahloomans@gmail.com>
2016-01-27 20:26:33 +0100

    Documented code

--- public/script/script.js
+++ public/script/script.js
@@ -1,17 +1,26 @@
 window.addEventListener("load", function () {
+
+  // if the add button is pressed, copy the data.
   $("#addButton").click(function () {
     var t = document.querySelector('#itemTamplate');
     var clone = document.importNode(t.content, true);
+    // tip: you can use `clone` instad of `document` to edit the data before
+    // adding it to .items (you can't you jquery for this!)
     document.querySelector(".items").appendChild(clone);
   });
 
+  // if the next button is pressed, send a GET request to `/results`
   $("#nextButton").click(function () {
+    // parse the data so it can be de-parsed by `/routs/result` using
+    // `getShoppingList()`
     var value = "";
     $(".items .item").each(function () {
       value += $(this).find("input").eq(0).val() + "@"
         +$(this).find("input").eq(1).val() + "!";
     });
 
+    // send a GET request (redirect) to /results and add the shopping list to
+    // it.
     window.location.href = "/results?value="+value;
 
   });
--- routes/results.js
+++ routes/results.js
@@ -19,16 +19,19 @@ function getShoppingList (req) {
 /* GET results page. */
 router.get('/', function(req, res, next) {
 
-  //res.render("results");
-
+  // take the ?value=Appel@1!Peer@3! data and turn it into the folowing object:
+  // [ {product: appel, amount: 1}, {product: peer, amount: 3} ]
   var shoppingList = getShoppingList(req);
 
+
+  // ask the database for the required data
   database.query('SELECT * FROM prices', function(err, prices, fields) {
 
     database.query('SELECT * FROM locations', function(err, locations, fields) {
 
       if (err) throw err;
 
+      // we have some text-json in the database, parse it into real json.
       for (var i = 0; i < prices.length; i++) {
         prices[i].json = JSON.parse(prices[i].json);
       }
@@ -39,24 +42,31 @@ router.get('/', function(req, res, next) {
       for (var i = 0; i < prices.length; i++) {
         totalPrice[i] = 0;
         for (var j = 0; j < shoppingList.length; j++) {
+
+          // does this shop have the required product?
           if (shoppingList[j].product in prices[i].json) {
+            // if so, do the price times the required amount and add it to
+            // `totalPrice`
             totalPrice[i] += prices[i].json[shoppingList[j].product]
                 * shoppingList[j].amount;
           }
           else {
+            // if not, blacklist it so we can remove it later
             blacklist.push(i);
           }
         }
       }
 
+      // remove the blacklisted shops, and subtract the `removed` count so we
+      // don't remove the wrong shops
       var removed = 0;
-
       blacklist.forEach(function (element) {
         totalPrice.splice(element-removed, 1);
         locations.splice(element-removed, 1);
         removed++;
       });
 
+      // built the data that will be send to the `views/results.jade`
       var data = [];
       for (var i = 0; i < locations.length; i++) {
         data[i] = {
@@ -67,6 +77,7 @@ router.get('/', function(req, res, next) {
         }
       }
 
+      // respond to the cliend
       res.render('results',{data: data});
 
     });
